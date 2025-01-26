cd736a1e0cb0670bc8a42d79f01846717b81ad74
AtulAbhiwan03 <139665307+TanuTechnologies@users.noreply.github.com>
2025-01-26 22:30:20 +0530

    multiplayer

--- Assets/_Game/_Scripts/_New Scripts/_Multiplayer/PhotonController.cs
+++ Assets/_Game/_Scripts/_New Scripts/_Multiplayer/PhotonController.cs
@@ -1,7 +1,11 @@
 using System;
+using System.Collections.Generic;
 using ExitGames.Client.Photon;
 using Photon.Pun;
 using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class PhotonController : MonoBehaviourPunCallbacks, IOnEventCallback
 {
@@ -9,7 +13,11 @@ public class PhotonController : MonoBehaviourPunCallbacks, IOnEventCallback
 
     #region Variables
 
+    private Dictionary<int, GameObject> PlayerList;
     
+    [Header("Player List")]
+    [SerializeField] internal GameObject PlayerlistPrefab;
+    [SerializeField] internal Transform PlayerlistParent;
 
     #endregion
     
@@ -36,6 +44,86 @@ public class PhotonController : MonoBehaviourPunCallbacks, IOnEventCallback
     {
        
     }
+    
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log(returnCode + message);
+    }
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to Internet ");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " is connected to master ");
+    }
+
+    public override void OnCreatedRoom()                                                           // Getting Room id
+    {
+        Debug.Log(PhotonNetwork.CurrentRoom + " is created!");
+        string roomId = PhotonNetwork.CurrentRoom.Name;
+        Debug.Log("Room id " + roomId);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("On room Joined");
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName + " Joined");
+        if (PlayerList == null)
+        {
+            PlayerList = new Dictionary<int, GameObject>();
+
+        }
+        foreach (Player p in PhotonNetwork.PlayerList)
+        {
+            GameObject playerList = Instantiate(PlayerlistPrefab, PlayerlistParent);
+            playerList.transform.GetChild(0).GetComponent<TMP_Text>().text = p.NickName;
+            if(p.ActorNumber == PhotonNetwork.LocalPlayer.ActorNumber)
+            {
+                playerList.transform.GetChild(1).gameObject.SetActive(true);
+            }
+            else
+            {
+                playerList.transform.GetChild(1).gameObject.SetActive(false );
+            }
+            PlayerList.Add(p.ActorNumber, playerList);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        GameObject playerList = Instantiate(PlayerlistPrefab, PlayerlistParent);
+        playerList.transform.GetChild(0).GetComponent<TMP_Text>().text = newPlayer.NickName;
+        if (newPlayer.ActorNumber == PhotonNetwork.LocalPlayer.ActorNumber)
+        {
+            playerList.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        else
+        {
+            playerList.transform.GetChild(1).gameObject.SetActive(false);
+        }
+        PlayerList.Add(newPlayer.ActorNumber, playerList);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Destroy(PlayerList[otherPlayer.ActorNumber]);
+        PlayerList.Remove(otherPlayer.ActorNumber);
+    }
+
+    public override void OnLeftRoom()
+    {
+        if (PlayerList.Count > 0)
+        {
+            foreach (GameObject obj in PlayerList.Values)
+            {
+                Destroy(obj);
+            }
+
+        }
+    }
+    
     
     #endregion
 
